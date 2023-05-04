ee8421e9cc1d51ae1500a49ce08651e1d1a424ee
nadeenay <70846138+nadeenay@users.noreply.github.com>
2023-05-04 19:39:26 +0300

    initial commit

---
+++ Extract_Features/utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+def mean_pixel_distance(left: np.ndarray, right: np.ndarray) -> float:
+    assert len(left.shape) == 2 and len(right.shape) == 2
+    assert left.shape == right.shape
+    num_pixels: float = float(left.shape[0] * left.shape[1])
+    return (np.sum(np.abs(left.astype(np.int32) - right.astype(np.int32))) / num_pixels)
