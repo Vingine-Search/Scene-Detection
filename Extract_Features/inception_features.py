03056848c67b05827d1995ff66672e0854b7088f
Omer Yacine <mariocynicys@gmail.com>
2023-06-18 10:33:51 +0300

    extract deep features using xception

---
+++ Extract_Features/inception_features.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+
+
+def DeepFeaturesExtractor():
+    inputs = tf.keras.layers.Input((299, 299, 3))
+    xcp_preprocessed_inputs = tf.keras.applications.xception.preprocess_input(inputs)
+    xcp = tf.keras.applications.Xception(
+        input_tensor=xcp_preprocessed_inputs,
+        # Don't include the top classification layer, as we will use this model for feature extraction.
+        include_top=False,
+        weights="imagenet",
+        pooling="avg",
+    )
+    # For sanity, we shouldn't run `model.fit` anyway.
+    xcp.trainable = False
+    features = tf.keras.layers.Flatten()(xcp.output)
+    return tf.keras.Model(inputs, features)
+
+
+def get_deep_features_for_key_frames(keyframe_files: list):
+    """Returns feature vectors based on `Xception` CNN network for the given list of (key) frames.
+    Args:
+        keyframe_files: A list of string file names for the frames to extract deep features from.
+                        (e.g. key_frames_files=['shot1_keyframe.jpg', 'shot2_keyframe.jpg', ...])
+    """
+    xcp_feat_ext = DeepFeaturesExtractor()
+    image_dimensions = xcp_feat_ext.input.shape[1:]
+    # A generator to load the key frames asynchronously.
+    def image_generator():
+        for keyframe_file in keyframe_files:
+            image = tf.image.decode_image(tf.io.read_file(keyframe_file))
+            resized_image = tf.image.resize_with_pad(
+                image, target_height=image_dimensions[0], target_width=image_dimensions[1])
+            yield resized_image
+    # Batch every one example in a dataset.
+    dataset = tf.data.Dataset.from_generator(
+        image_generator, output_shapes=image_dimensions, output_types=(tf.float32)).batch(1)
+    return xcp_feat_ext.predict(dataset)
+
+def test_feat_ext():
+    # NOTE1: The norm used here might not be the distance equation used in the OSG paper.
+    # NOTE2: The distance between animals of different breeds are as big as the distance between different animals,
+    #        (e.g. a Rottweiler and a German Shepherd are as distant as a Rottweiler and a Tabby cat).
+    #        This might not be an issue since changes in the characters of the scenes (and their breeds) should indicate
+    #        a change in the scene. Breed here is just an example of how specific the Xception model is.
+    import numpy as np
+    images = ['cat1.jpg', 'cat2.jpg', 'dog1.jpg', 'dog2.jpg', 'shark.jpg']
+    ret = get_deep_features_for_key_frames(images)
+    for one in range(len(images)):
+        for two in range(one + 1, len(images)):
+            print(images[one], images[two], np.linalg.norm(ret[one] - ret[two]))
+
+test_feat_ext()
